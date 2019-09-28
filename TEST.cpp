b3b19e3444c07498025ce6d02d9ea030fdf25fd0
Mahdi <Mahdi.Alehdaghi@gmail.com>
2019-09-28 15:28:11 +0330

    Writes the first test! Tests the creation of game object and player

---
+++ TEST.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mahdi on 28.09.19.
+//
+
+void test1_test_objects() {
+    GameObject o; // must rise an compilation error due as abstraction
+    Player p(); // Default constructor
+    Player p2("mahdi", 40, 2); // Constructor with configuration, the name, health and, power of each attack
+}
