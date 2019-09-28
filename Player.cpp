caa4ec67aa89273135d1d4c6e8b5c9839e18c16f
Mahdi <Mahdi.Alehdaghi@gmail.com>
2019-09-28 15:44:34 +0330

    Passes the test Player Creation

---
+++ Player.cpp
@@ -0,0 +1,17 @@
+//
+// Created by mahdi on 28.09.19.
+//
+
+#include "Player.h"
+
+Player::Player() : GameObject() {
+
+}
+
+Player::Player(std::string name, int health, int power) : GameObject(name, health, power) {
+
+}
+
+void Player::run() {
+
+}
--- TEST.cpp
+++ TEST.cpp
@@ -2,8 +2,11 @@
 // Created by mahdi on 28.09.19.
 //
 
+#include "GameObject.h"
+#include "Player.h"
+
 void test1_test_objects() {
-    GameObject o; // must rise an compilation error due as abstraction
+    //GameObject o; // must rise an compilation error due as abstraction
     Player p(); // Default constructor
     Player p2("mahdi", 40, 2); // Constructor with configuration, the name, health and, power of each attack
 }
